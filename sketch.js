8fe5f67d48623b27c97cfc560bc36664687ec963
PRaBh-2021 <ps.tiwana10@gmail.com>
2020-08-16 10:14:26 +0530

    platform

--- sketch.js
+++ sketch.js
@@ -5,17 +5,18 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
 
 function setup(){
-    var canvas = createCanvas(1200,400);
+    var canvas = createCanvas(1200,400);             
     engine = Engine.create();
     world = engine.world;
 
-    
+    platform = new ground(100,385,300,400);
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -46,6 +47,7 @@ function draw(){
     box1.display();
     box2.display();
     ground.display();
+    platform.display();
     pig1.display();
     log1.display();
 
@@ -59,4 +61,5 @@ function draw(){
     log5.display();
 
     bird.display();
+
 }
